c6432acfa66d235d298b2fc0cd64b1fe92e790ef
ThePhalanxProject <ayman@thephalanxproject.fr>
2026-09-07 16:32:55 +0200

    Filter measurement history by device for consumption prediction

--- crud.py
+++ crud.py
@@ -6,25 +6,22 @@ from schemas import Measurement
 
 
 def create_measurement(db: Session, measurement: Measurement):
-
     new_entry = MeasurementModel(
         device_id=measurement.device_id,
         weight=measurement.weight,
         battery=measurement.battery_percentage,
-        timestamp=datetime.utcnow()
+        timestamp=datetime.utcnow(),
     )
 
     db.add(new_entry)
     db.commit()
     db.refresh(new_entry)
-
     return new_entry
 
 
-def get_measurements(db: Session):
+def get_measurements(db: Session, device_id: str | None = None):
+    query = db.query(MeasurementModel)
+    if device_id is not None:
+        query = query.filter(MeasurementModel.device_id == device_id)
 
-    return (
-        db.query(MeasurementModel)
-        .order_by(MeasurementModel.timestamp.desc())
-        .all()
-    )
+    return query.order_by(MeasurementModel.timestamp.desc()).all()
